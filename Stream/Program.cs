f0e9964c32eeb6c29a9086b2384dbeda7acb2e81
Alina517 <51297046+Alina517@users.noreply.github.com>
2021-06-14 22:23:13 +0300

    Add files via upload

---
+++ Stream/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("text.txt");//открываем файл для чтения
+            string readText = file.ReadToEnd();//считываем все данные из файла и записываем в readText
+            file.Close();//закрывем файл
+
+            string[] newText = Regex.Split(readText, "\n");//делим считанные данные на строки
+            for(int i = 0; i < newText.Length; i++)//проходим по массыву данных
+            {
+                for(int j = 1; j < newText[i].Length; j++)
+                {                  
+                    newText[i] = newText[i].Remove(j, 1);  // удаляем четные элементы каждой строки                                
+                }
+            }
+            StreamWriter fileout = new StreamWriter("text1.txt", false, System.Text.Encoding.Default);//открываем файл для записи
+            foreach(string b in newText)
+            {
+                fileout.WriteLine(b);// записывем полученные данные в файл
+            }
+            fileout.Close();// закрываем файл
+            Console.WriteLine("Данные записаны в файл text1.txt");//вывод сообщения в консоль
+        }
+    }
+}
